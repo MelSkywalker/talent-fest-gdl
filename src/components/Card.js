4b9c7e4ff0475e103e639cdee070e1e5796fa089
MEL <melisa.quc@gmail.com>
2019-08-06 07:00:58 -0500

    map in react with current location school markers

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = (name, address) => {
+    return(
+    <div>
+        {/* <h3>{name}</h3>
+        <p>{address}</p> */}
+        <h3>{name}</h3>
+        <p>{address}</p>
+    </div>
+    )
+};
+
+export default Card;
